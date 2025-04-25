b77b79f61f61f0486592702aa49616ebc30f6f74
David Quispe <153945051+dayi-ley@users.noreply.github.com>
2025-04-25 12:20:03 -0500

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -1,26 +1,23 @@
-const { Client } = require('whatsapp-web.js');
-
 module.exports = {
     name: 'loop',
     aliases: [],
-    /** 
-     * @param {Client} client 
-     * @param {} message 
-     * @param {String[]} args 
+    /**
+     * @param {Client} client
+     * @param {Message} message
+     * @param {String[]} args
      */
-    run: async (client, message, args) => {
-        // split the message by spaces and get the last element as the number of times to loop
-        const times = parseInt(args.pop())
-        // join the rest of the message
-        const msg = args.join(' ')
-        // check if the message is empty
-        if (!msg) return message.reply('Debes ingresar un mensaje.')
-        // check if the number of times is a number
-        if (isNaN(times)) return message.reply('Debes ingresar un número.')
-        // loop the message the number of times
-        for (let i = 0; i < times; i++) {
-            message.reply(msg)
-        }
+    execute: async (client, message, args) => {
+        if (args.length < 2) return message.reply('Uso: !loop <veces> <mensaje>');
+        
+        const times = parseInt(args[0]);
+        const msg = args.slice(1).join(' ');
 
+        if (isNaN(times) return message.reply('El primer argumento debe ser un número.');
+        if (times > 10) return message.reply('Máximo 10 repeticiones por seguridad.');
+
+        for (let i = 0; i < times; i++) {
+            await message.reply(msg);
+            if (i < times - 1) await new Promise(resolve => setTimeout(resolve, 1000));
+        }
     }
-}
+};
